d94e836a601dd932b26f93f8c07026b969a16c01
APCS2nd <APCS2nd@MHS-LT9-04>
2019-08-21 12:17:50 -0700

    Complete Hello World Project

---
+++ HolaMundoProyecto/src/HolaMundo.java
@@ -0,0 +1,12 @@
+/* Harrison Dong
+ * August 19, 2019
+ * Prints a message to the screen
+ * Hello World is the iconic "first" program in a new lang
+ */
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
