e9c70aa7faddd6d86bf1559979e2282f3b1333c2
APCS2nd <APCS2nd@MHS-LT9-04>
2019-09-04 12:08:00 -0700

    Access Calculate 1st Save So far only calls the Calculate.square class.

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,11 @@
+/*Harrison Dong
+ * August 29, 2019 4th Period
+ * Calls math methods from Calculate class
+ * Client code or runner code*/
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(12));
+	}
+
+}
