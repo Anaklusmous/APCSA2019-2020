262d95d0a2b1d1868a7dfc231f3c2e0b5fa85173
APCS2nd <APCS2nd@MHS-LT9-04>
2019-11-06 12:33:28 -0800

    ArraysLab3 Mudda Finished

--- ArraysLab3/src/ArraysLab3.java
+++ ArraysLab3/src/ArraysLab3.java
@@ -1,12 +1,13 @@
-
+import java.util.*;
 public class ArraysLab3 {
 	//Write a method sum that accepts two arrays of integers arr1 and arr2 and returns an array of integers, in which every element is the sum of the elements at that index for the arrays arr1 and arr2. You can assume arrays arr1 and arr2 have at least one element each and are the same length.
 
 	public static int[] sum(int[] arr1, int[] arr2) {
-		for (int i = 0; i < arr1.length; i++) {
-			arr1[1] += arr2[i];
+		int[] sumArr = new int[arr1.length];
+		for (int i = 0; i < sumArr.length; i++) {
+			sumArr[i] = arr1[i] + arr2[i];
 		}
-		return arr1;
+		return sumArr;
 	}
 
 	//2) Write a method append that accepts an array of integers arr of length n and an integer num, and returns an array of integers of length n+1 that consists of the elements of arr with num appended to the end.  You can assume array arr has at least one element.
@@ -42,13 +43,26 @@ public class ArraysLab3 {
 	//4) Write a method sumEven that accepts an array of integers arr and returns an integer containing the sum of the elements at the even indices of arr.  (That means elements at indices 0,2,4,6,8.)  You can assume arr has at least one element.
 
 	public static int sumEven(int[] arr) {
-		
+		int sum = 0;
+		for(int i = 0; i < arr.length; i += 2) {
+			sum += arr[i];
+		}
+		return sum;
 	}
 
 
 	//5) Write a method rotateRight that accepts an array of integers arr and does not return a value.  The rotateRight method moves each element of arr one index to the right (element 0 goes to element 1, element 1 goes to element 2, …, element n-1 goes to element 0).  You can assume arr has at least one element.
 
-	public static void rotateRight(int[] arr) {}
+	public static void rotateRight(int[] arr) {
+		int temp1 = arr[0];
+		int temp2 = 0;
+		for(int i = 0; i < arr.length - 1; i++) {
+			temp2 = arr[i + 1];
+			arr[i + 1] = temp1;
+			temp1 = temp2;
+		}
+		arr[0] = temp1;
+	}
 
 	//6) Write a method main that will 
 
@@ -71,6 +85,19 @@ public class ArraysLab3 {
 	sumOfEvens (an int you don’t need Arrays.toString)
 	a1*/
 	public static void main(String[] args) {
-			
-		}
+		int[] a1 = {5, 10, 15, 20, 25, 30, 35, 40};
+		int[] a2 = {7, 14, 21, 28, 35, 42, 49, 56};
+		int[] sumArr = sum(a1, a2);
+		int appendNum = 200;
+		int[] appendArr = append(a1, appendNum);
+		int removeIdx = 5;
+		int[] removeArr = remove(a2, removeIdx);
+		int sumOfEvens = sumEven(appendArr);
+		rotateRight(a1);
+		System.out.println(Arrays.toString(sumArr));
+		System.out.println(Arrays.toString(appendArr));
+		System.out.println(Arrays.toString(removeArr));
+		System.out.println(sumOfEvens);
+		System.out.println(Arrays.toString(a1));
+	}
 }
